570b8e4ce686b2645e922b664e5b988cc088170e
innovin <innovinanuonye@gmail.com>
2023-03-23 14:58:21 +0100

    another update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import socket from "socket.io";
+// import {Server} from "socket.io";
+const socket = require('socket.io');
 import App from "./App";
 const port = 1000;
 new App(port)
 .run()
-.then((server) => {
-    const io = new socket.Server(server);
-    // console.log(io);
+.then((app) => {
+    try {
+        const io = new socket.Server(app);
+        io.on('connection', () => {
+            console.log('hi')
+            console.log('socket up and running')
+        })
+
+    } catch (e) {
+        return console.error('A socket error occurred \n', e);
+    }
 })
 .catch((err) => {
     console.error("Something went wrong \n", err);
