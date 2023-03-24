62617b65f2ca3ebd8aa4f00e22cf3fbc1f937680
innovin <innovinanuonye@gmail.com>
2023-03-24 21:46:44 +0100

    frontend update

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,23 @@ new App(port)
 .run()
 .then((app) => {
     try {
-        const io = new socket.Server(app);
-        io.on('connection', () => {
-            console.log('hi')
-            console.log('socket up and running')
+        const io = socket(app);
+        // io.listen(3000);
+       // g('hdhehhje')
+        // })
+
+        // add a setTimeout to the document
+        io.on('connection', (socket: any) => {
+            // socket.
+            console.log("new socket: ", socket.id);
+            socket.on('new text', (state: any) => {
+
+                console.log('new text put in ', state);
+                socket.broadcast.emit('new text', state)
+            })
+
         })
+
 
     } catch (e) {
         return console.error('A socket error occurred \n', e);
